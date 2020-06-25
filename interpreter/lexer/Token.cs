812d899503b7fe8e9334d40260e208a03f615d25
henry <hps1945@live.co.uk>
2020-06-25 21:54:49 +0100

    added token and token types

---
+++ interpreter/lexer/Token.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace interpreter.lexer
+{
+    class Token
+    {
+        public string TokenType {get; set;}
+        public string Literal {get; set;}
+    }
+}
