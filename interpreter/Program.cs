3b37beba739d7cdf3f2dc0dad2d4e6ff91e70bdd
henry <hps1945@live.co.uk>
2020-06-25 18:27:25 +0100

    moved to dotnet

---
+++ interpreter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace interpreter
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Run("test.jpg");
+        }
+
+        private static async Task Run(string fileName)
+        {
+            Console.WriteLine($"Given the file: {fileName}");
+        }
+    }
+}
--- src/Main.kt
+++
@@ -1,7 +0,0 @@
-import hps.interpreter.models.Token
-
-fun main(args: Array<String>){
-    val t = Token("yoyo")
-
-    println(t.setValue)
-}
